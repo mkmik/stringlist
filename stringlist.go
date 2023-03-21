4ba2a9e71438aef5eb852d7194a9049871a239d4
Marko Mikulicic <mmikulicic@gmail.com>
2023-03-21 11:57:44 +0100

    Add stringlist.Var

--- stringlist.go
+++ stringlist.go
@@ -1,35 +1,52 @@
 /*
-	Package stringlist implements a custom "stringlist" flag
-	It accepts comma separated values and repeated flag occurrence.
-	Leading and trailing whitespace will be trimmed.
+Package stringlist implements a custom "stringlist" flag
+It accepts comma separated values and repeated flag occurrence.
+Leading and trailing whitespace will be trimmed.
 
-	Usage:
+Usage:
 
-		var (
-			myFlag stringlist.Value
-		)
+	var (
+		myFlag stringlist.Value
+	)
 
-		func init() {
-			flag.Var(&myFlag, "myflag", "a list of strings")
+	func init() {
+		flag.Var(&myFlag, "myflag", "a list of strings")
+	}
+
+Or:
+
+	var (
+		myFlag = stringlist.Flag("myflag", "a list of strings")
+	)
+
+	func foo() {
+		for _, i := range *myFlag {
+			// note: you need to dereference the slice
 		}
+	}
 
-	Or:
+Or:
 
-		var (
-			myFlag = stringlist.Flag("myflag", "a list of strings")
-		)
+	var (
+		myFlag []string
+	)
 
-		func foo() {
-			for _, i = range *myFlag {
-				// note: you need to dereference the slice
-			}
+	func init() {
+		stringlist.Var(&myFlag, "myflag", "a list of strings")
+	}
+
+	func main() {
+		flag.Parse()
+		for _, i := range myFlag {
+			// note: no need to dereference the slice
 		}
+	}
 
-	If you invoke it with:
+If you invoke it with:
 
-		--myflag a,b --myflag c --myflag "c, d"
+	--myflag a,b --myflag c --myflag "c, d"
 
-	myFlag will contain: []string{"a", "b", "c", "d"}
+myFlag will contain: []string{"a", "b", "c", "d"}
 */
 package stringlist
 
@@ -59,4 +76,29 @@ func Flag(name string, usage string) *Value {
 	var r Value
 	flag.Var(&r, name, usage)
 	return &r
+}
+
+type Slice struct{ slice *[]string }
+
+// Set implements the flag.Value interface
+func (r Slice) Set(args string) error {
+	v := Value(*r.slice)
+	if err := v.Set(args); err != nil {
+		return err
+	}
+	*r.slice = v
+	return nil
+}
+
+func SliceRef(s *[]string) Slice {
+	return Slice{slice: s}
+}
+
+func (r Slice) String() string {
+	v := Value(*r.slice)
+	return v.String()
+}
+
+func Var(s *[]string, name string, usage string) {
+	flag.Var(SliceRef(s), name, usage)
 }
